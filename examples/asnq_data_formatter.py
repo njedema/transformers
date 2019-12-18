f9b3071df846b1cdfb755505ddc432b32506c576
jedem <jedem@amazon.com>
2019-12-17 20:04:01 -0800

    Fixing syntax errors made on a flight :facepalm:

--- examples/asnq_data_formatter.py
+++ examples/asnq_data_formatter.py
@@ -1,14 +1,13 @@
 import jsonlines
 import os
 import argparse
+import csv
+from bs4 import BeautifulSoup
 
 
 DATA_DIR = '/Users/jedem/Kaggle/data/'
-REQUIRED_FIELDS = ['example_id',
-                   'question_text',
-                   'document_text',
-                   'long_answer_candidates',
-                   ]
+REQUIRED_INPUT_FIELDS = ['example_id', 'question_text', 'document_text', 'long_answer_candidates',]
+ASNQ_OUTPUT_FIELDS = ['question', 'answer', 'annotation']
 
 
 def read_file(file_path: str):
@@ -16,7 +15,7 @@ def read_file(file_path: str):
     with jsonlines.open(file_path) as reader:
         for obj in reader:
             missing = []
-            for field in REQUIRED_FIELDS:
+            for field in REQUIRED_INPUT_FIELDS:
                 if field not in obj:
                     missing.append(field)
             if missing:
@@ -27,11 +26,45 @@ def read_file(file_path: str):
 
 
 def write_asnq_format(file_path: str, lines):
-    pass
+    """
+    :param file_path: Output file path
+    :param lines: Generator of lines to write out
+    Writes a .tsv file out with one line per candidate in line.clean_candidates
+    """
+    LINES_LOG_INTERVAL = 5000
+    with open(file_path, 'a+') as output_file:
+        writer = csv.DictWriter(output_file, fieldnames=ASNQ_OUTPUT_FIELDS, delimiter=" ")  # delimiter is a tab -> .tsv
+        for line in enumerate(lines):
+            clean_candidates = line[1].get('clean_candidates')
+            question = line[1].get('question_text')
+            annotation = line[1].get('annotation')
+            for candidate in clean_candidates:
+                output_line = {'question': question, 'answer': candidate}
+                output_line['annotation'] = annotation if annotation else None
+                writer.writerow(output_line)
+            if line[0] % LINES_LOG_INTERVAL == 0:
+                print('Appended {} lines to {}'.format(LINES_LOG_INTERVAL, file_path))
 
 
-def clean_lines(lines):
-    return []
+def clean_candidates(lines):
+    """
+    :param lines: A generator of dictionaries representing rows from the input file
+    :return: A generator of the same lines with clean_candidates in place of candidates
+    The ASNQ transfer task doesn't use Lowercase and doesn't strip punctuation or numeric. The only cleaning necessary
+    is simply to extract the paragraph from the HTML representation of the candidate.
+    """
+    for line in lines:
+        candidates = line.pop('candidates')
+        clean = []
+        for candidate in candidates:
+            soup = BeautifulSoup(candidate['text'], features="html.parser")
+            # there may be multiple paragraphs in a candidate; pull out the text and aggregate them into one candidate
+            paragraphs = soup.find_all('p')
+            clean_candidate = " ".join(sentence.get_text() for sentence in paragraphs)
+            if clean_candidate:
+                clean.append(clean_candidate)
+        line['clean_candidates'] = clean
+        yield line
 
 
 def extract_candidate_strings(lines):
@@ -49,24 +82,22 @@ def extract_candidate_strings(lines):
 
 
 def main(**kwargs):
-    lines = read_file(kwargs.get('file'))
+    lines = read_file(os.path.join(DATA_DIR, kwargs.get('file')))
     candidates = extract_candidate_strings(lines)
-    for line in candidates:
-        print(line.get('candidates'))
-    #cleaned_candidates = clean_lines(candidates)
-    #write_asnq_format(kwargs.get('output_file'), cleaned_candidates)
+    cleaned_lines = clean_candidates(candidates)
+    write_asnq_format(kwargs.get('output_file'), cleaned_lines)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('kaggle_data_formatter')
-    parser.add_argument('-f', '--file', type=str,
-                        help='Path of a file to parse into ASNQ format')
+    parser.add_argument('-f', '--file', type=str, required=True,
+                        help='Path of a file to parse into ASNQ format; must live in DATA_DIR {}'.format(DATA_DIR))
     parser.add_argument('--output_file', type=str,
-                        help='Path to write the ASNQ formatted file to')
+                        help='Path to write the ASNQ formatted file to; must live in DATA_DIR {}'.format(DATA_DIR))
     arg_dict = vars(parser.parse_args())
     if not arg_dict.get('output_file'):
         file_name = arg_dict.get('file').split('/')[-1]
-        file_name_no_suffix = file_name.split('.')[-1]
+        file_name_no_suffix = file_name.split('.')[0].replace("-", "_")
         _output_file = os.path.join(DATA_DIR, '{}_ansq_formatted.tsv'.format(file_name_no_suffix))
         arg_dict['output_file'] = _output_file
 
